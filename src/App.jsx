3b1f12d8e64c5fe41d421b59f05d0e7f6ad5156a
hakhan1999 <khanhassaan961@gmail.com>
2025-07-30 11:18:44 +0500

    Project Setup and basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "./components/ui/button";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Button>Click Me</Button>
+      <Outlet />
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import SignIn from "./auth/sign-in/SignIn";
+import Home from "./home/Home";
+import Dashboard from "./dashboard/Dashboard";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
